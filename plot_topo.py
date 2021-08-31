ec530caa969a65229446acd9207a900eee660164
Caio J Ruman <caioruman@gmail.com>
2021-08-31 14:59:56 -0400

    Adding the globe plot

--- plot_topo.py
+++ plot_topo.py
@@ -26,6 +26,30 @@ class FixPointNormalize(matplotlib.colors.Normalize):
     x, y = [self.vmin, self.sealevel, self.vmax], [0, self.col_val, 1]
     return np.ma.masked_array(np.interp(value, x, y))
 
+def geo_idx(dd, dd_array, type="lat"):
+  '''
+    search for nearest decimal degree in an array of decimal degrees and return the index.
+    np.argmin returns the indices of minium value along an axis.
+    so subtract dd from all values in dd_array, take absolute value and find index of minimum.
+    
+    Differentiate between 2-D and 1-D lat/lon arrays.
+    for 2-D arrays, should receive values in this format: dd=[lat, lon], dd_array=[lats2d,lons2d]
+  '''
+  if type == "lon" and len(dd_array.shape) == 1:
+    dd_array = np.where(dd_array <= 180, dd_array, dd_array - 360)
+
+  if (len(dd_array.shape) < 2):
+    geo_idx = (np.abs(dd_array - dd)).argmin()
+  else:
+    if (dd_array[1] < 0).any():
+      dd_array[1] = np.where(dd_array[1] <= 180, dd_array[1], dd_array[1] - 360)
+
+    a = abs( dd_array[0]-dd[0] ) + abs(  np.where(dd_array[1] <= 180, dd_array[1], dd_array[1] - 360) - dd[1] )
+    i,j = np.unravel_index(a.argmin(), a.shape)
+    geo_idx = [i,j]
+
+  return geo_idx
+
 def main():
 
   # Setting the colormap for the terrain  
@@ -92,6 +116,69 @@ def main():
                                                                 add_colorbar=False, cmap=sea_map)
   plt.title('Topography: New Brunswick')
   plt.savefig('topo_newbrunswick_v2.png', dpi=150, pad_inches=0.0, bbox_inches='tight')
+
+  plt.close()
+  
+  # Domain on the Globe
+
+  plt.figure(figsize=(14,10))
+  proj = ccrs.Orthographic(central_longitude=-98, central_latitude=45.0)
+  ax = plt.axes(projection=proj)
+  #ax.add_feature(cfeature.LAND)
+  #ax.add_feature(cfeature.OCEAN)
+  ax.coastlines(resolution='50m', linewidth=0.5)
+  ax.add_feature(cfeature.BORDERS.with_scale("50m"), linewidth=width)
+  ax.add_feature(cfeature.STATES.with_scale("50m"), linewidth=width/2)
+  #ax.add_feature(cfeature.COASTLINE) #, edgecolor="brown")
+  ax.stock_img()
+  ax.gridlines(color="#666")
+
+  # Domain
+  ii = [0,-1]
+
+  for i in ii:
+    plt.plot(tp.XLONG[0,i,:], tp.XLAT[0,i,:],
+            color='red', linewidth=2,
+            transform=ccrs.Geodetic(),
+            )
+    plt.plot(tp.XLONG[0,:,i], tp.XLAT[0,:,i],
+            color='red', linewidth=2,
+            transform=ccrs.Geodetic(),
+            )
+
+  # NB Area of Interest
+  lat = 45
+  lon = -70.5
+  i1, j1 = geo_idx([lat, lon], np.array([tp.XLAT[0], tp.XLONG[0]]))
+  lat = 48.2
+  lon = -61.5
+  i2, j2 = geo_idx([lat, lon], np.array([tp.XLAT[0], tp.XLONG[0]]))
+
+  ii = [i1, j1]
+
+  #for i in ii:
+  plt.plot(tp.XLONG[0,i1,j1:j2], tp.XLAT[0,i1,j1:j2],
+          color='red', linewidth=1,
+          transform=ccrs.Geodetic(),
+          )
+  plt.plot(tp.XLONG[0,i1:i2,j1], tp.XLAT[0,i1:i2,j1],
+          color='red', linewidth=1,
+          transform=ccrs.Geodetic(),
+          )
+
+  plt.plot(tp.XLONG[0,i1:i2,j2], tp.XLAT[0,i1:i2,j2],
+          color='red', linewidth=1,
+          transform=ccrs.Geodetic(),
+          )
+  plt.plot(tp.XLONG[0,i2,j1:j2], tp.XLAT[0,i2,j1:j2],
+          color='red', linewidth=1,
+          transform=ccrs.Geodetic(),
+          )  
+
+  plt.tight_layout()
+  plt.savefig('domain_globe.png')
+  plt.show()
+  plt.close()
 
 
 
